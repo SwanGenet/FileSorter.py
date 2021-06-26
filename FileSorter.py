4d1dc57f22cb336cff9212ed89057fa434cd5100
SwanGenet <38185601+SwanGenet@users.noreply.github.com>
2021-06-26 03:04:14 +0200

    Update FileSorter.py
    Added .rar ...

--- FileSorter.py
+++ FileSorter.py
@@ -1,43 +1,48 @@
 import os
 import shutil
 
-cwd = os.getcwd() #Current Working Directory
-ls = os.listdir(cwd) #List of files in cwd
-    
-directory = ['img','video','txt','pdf','audio'] #Different dir to create
-parent_dir = os.getcwd() 
-mode = 0o666   
+cwd = os.getcwd()  # Current Working Directory
+ls = os.listdir(cwd)  # List of files in cwd
+
+directory = ['img', 'video', 'txt', 'pdf',
+             'audio', 'rar']  # Different dir to create
+parent_dir = os.getcwd()
+mode = 0o666
 
 i = 0
 while i < len(directory):
     if directory[i] not in ls:
-        path = os.path.join(parent_dir, directory[i]) 
-        os.makedirs(path, mode) #Creation of the dir
+        path = os.path.join(parent_dir, directory[i])
+        os.makedirs(path, mode)  # Creation of the dir
     i += 1
 
-img_ext = ('png','jpg','svg','jpeg','gif','tiff','psd','eps','ai','indd','raw')
-txt_ext = ('txt','rtf','docx','doc','odt')
-video_ext = ('avi','mkv','mp4','mov','flv','wvm','webm')
-audio_ext = ('mp3','flac','wav','aac','m4v','wma')
+img_ext = ('png', 'jpg', 'svg', 'jpeg', 'gif',
+           'tiff', 'psd', 'eps', 'ai', 'indd', 'raw')
+txt_ext = ('txt', 'rtf', 'docx', 'doc', 'odt')
+video_ext = ('avi', 'mkv', 'mp4', 'mov', 'flv', 'wvm', 'webm')
+audio_ext = ('mp3', 'flac', 'wav', 'aac', 'm4v', 'wma')
+rar_ext = ('zip', 'rar', 'tar', 'gz')
 
 i = 0
 while i < len(ls):
     if ls[i].lower().endswith(img_ext):
-        shutil.move(ls[i], os.getcwd()+'/img') #Move files to /img
+        shutil.move(ls[i], os.getcwd()+'/img')  # Move files to /img
 
     elif ls[i].endswith(txt_ext):
-        shutil.move(ls[i], os.getcwd()+'/txt') #Move files to /txt
+        shutil.move(ls[i], os.getcwd()+'/txt')  # Move files to /txt
 
     elif ls[i].endswith('.pdf'):
-        shutil.move(ls[i], os.getcwd()+'/pdf') #Move files to /pdf
+        shutil.move(ls[i], os.getcwd()+'/pdf')  # Move files to /pdf
 
     elif ls[i].endswith('.exe'):
-        shutil.move(ls[i], os.getcwd()+'/exe') #Move files to /exe
+        shutil.move(ls[i], os.getcwd()+'/exe')  # Move files to /exe
 
     elif ls[i].endswith(audio_ext):
-        shutil.move(ls[i], os.getcwd()+'/audio') #Move files to /audio
+        shutil.move(ls[i], os.getcwd()+'/audio')  # Move files to /audio
 
     elif ls[i].endswith(video_ext):
-        shutil.move(ls[i], os.getcwd()+'/video') #Move files to /vid
+        shutil.move(ls[i], os.getcwd()+'/video')  # Move files to /vid
+
+    elif ls[i].endswith(rar_ext):
+        shutil.move(ls[i], os.getcwd()+'/rar')  # Move files to /rar
     i += 1
- 
